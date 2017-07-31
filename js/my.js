914f1889478335ec09a237c1dddbbe6d91551ad2
Kim <smert4ekistam@gmail.com>
2017-07-31 17:38:47 +0300

    upvork

--- js/my.js
+++ js/my.js
@@ -1,39 +1,417 @@
+var countries = {
+    "AF": "Afghanistan",
+    "AL": "Albania",
+    "DZ": "Algeria",
+    "AO": "Angola",
+    "AG": "Antigua and Barbuda",
+    "AR": "Argentina",
+    "AM": "Armenia",
+    "AU": "Australia",
+    "AT": "Austria",
+    "AZ": "Azerbaijan",
+    "BS": "Bahamas",
+    "BH": "Bahrain",
+    "BD": "Bangladesh",
+    "BB": "Barbados",
+    "BY": "Belarus",
+    "BE": "Belgium",
+    "BZ": "Belize",
+    "BJ": "Benin",
+    "BT": "Bhutan",
+    "BO": "Bolivia",
+    "BA": "Bosnia and Herzegovina",
+    "BW": "Botswana",
+    "BR": "Brazil",
+    "BN": "Brunei",
+    "BG": "Bulgaria",
+    "BF": "Burkina Faso",
+    "BI": "Burundi",
+    "KH": "Cambodia",
+    "CM": "Cameroon",
+    "CA": "Canada",
+    "CV": "Cape Verde",
+    "CF": "Central African Republic",
+    "TD": "Chad",
+    "CL": "Chile",
+    "CN": "China",
+    "CO": "Colombia",
+    "KM": "Comoros",
+    "CR": "Costa Rica",
+    "HR": "Croatia",
+    "CU": "Cuba",
+    "CY": "Cyprus",
+    "CZ": "Czech Republic",
+    "CD": "Democratic Republic of the Congo",
+    "DK": "Denmark",
+    "DJ": "Djibouti",
+    "DM": "Dominica",
+    "DO": "Dominican Republic",
+    "TL": "East Timor",
+    "EC": "Ecuador",
+    "EG": "Egypt",
+    "SV": "El Salvador",
+    "GQ": "Equatorial Guinea",
+    "ER": "Eritrea",
+    "EE": "Estonia",
+    "ET": "Ethiopia",
+    "FJ": "Fiji",
+    "FI": "Finland",
+    "FR": "France",
+    "GF": "French Guiana",
+    "GA": "Gabon",
+    "GM": "Gambia",
+    "GE": "Georgia",
+    "DE": "Germany",
+    "GH": "Ghana",
+    "GR": "Greece",
+    "GD": "Grenada",
+    "GP": "Guadeloupe",
+    "GT": "Guatemala",
+    "GN": "Guinea",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HT": "Haiti",
+    "HN": "Honduras",
+    "HU": "Hungary",
+    "IS": "Iceland",
+    "IN": "India",
+    "ID": "Indonesia",
+    "IR": "Iran",
+    "IQ": "Iraq",
+    "IE": "Ireland",
+    "IL": "Israel",
+    "IT": "Italy",
+    "CI": "Ivory Coast",
+    "JM": "Jamaica",
+    "JP": "Japan",
+    "JO": "Jordan",
+    "KZ": "Kazakhstan",
+    "KE": "Kenya",
+    "KI": "Kiribati",
+    "KW": "Kuwait",
+    "KG": "Kyrgyzstan",
+    "LA": "Laos",
+    "LV": "Latvia",
+    "LB": "Lebanon",
+    "LS": "Lesotho",
+    "LR": "Liberia",
+    "LY": "Libya",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "MO": "Macao",
+    "MK": "Macedonia",
+    "MG": "Madagascar",
+    "MW": "Malawi",
+    "MY": "Malaysia",
+    "MV": "Maldives",
+    "ML": "Mali",
+    "MT": "Malta",
+    "MH": "Marshall Islands",
+    "MR": "Mauritania",
+    "MU": "Mauritius",
+    "MX": "Mexico",
+    "MD": "Moldova",
+    "MN": "Mongolia",
+    "ME": "Montenegro",
+    "MA": "Morocco",
+    "MZ": "Mozambique",
+    "MM": "Myanmar",
+    "NA": "Namibia",
+    "NR": "Nauru",
+    "NP": "Nepal",
+    "NL": "Netherlands",
+    "NZ": "New Zealand",
+    "NI": "Nicaragua",
+    "NE": "Niger",
+    "NG": "Nigeria",
+    "NO": "Norway",
+    "OM": "Oman",
+    "PK": "Pakistan",
+    "PW": "Palau",
+    "PA": "Panama",
+    "PG": "Papua New Guinea",
+    "PY": "Paraguay",
+    "PE": "Peru",
+    "PH": "Philippines",
+    "PL": "Poland",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "CG": "Republic of the Congo",
+    "RO": "Romania",
+    "RU": "Russia",
+    "RW": "Rwanda",
+    "KN": "Saint Kitts and Nevis",
+    "LC": "Saint Lucia",
+    "VC": "Saint Vincent and the Grenadines",
+    "WS": "Samoa",
+    "SM": "San Marino",
+    "ST": "Sao Tome and Principe",
+    "SA": "Saudi Arabia",
+    "SN": "Senegal",
+    "RS": "Serbia",
+    "SC": "Seychelles",
+    "SL": "Sierra Leone",
+    "SG": "Singapore",
+    "SK": "Slovakia",
+    "SI": "Slovenia",
+    "ZA": "South Africa",
+    "KR": "South Korea",
+    "ES": "Spain",
+    "LK": "Sri Lanka",
+    "SD": "Sudan",
+    "SR": "Suriname",
+    "SZ": "Swaziland",
+    "SE": "Sweden",
+    "CH": "Switzerland",
+    "TW": "Taiwan",
+    "TJ": "Tajikistan",
+    "TZ": "Tanzania",
+    "TH": "Thailand",
+    "TG": "Togo",
+    "TO": "Tonga",
+    "TT": "Trinidad and Tobago",
+    "TN": "Tunisia",
+    "TR": "Turkey",
+    "TM": "Turkmenistan",
+    "TV": "Tuvalu",
+    "UG": "Uganda",
+    "UA": "Ukraine",
+    "AE": "United Arab Emirates",
+    "GB": "United Kingdom",
+    "US": "United States",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+    "VU": "Vanuatu",
+    "VE": "Venezuela",
+    "VN": "Vietnam",
+    "YE": "Yemen",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe"
+};
 
-var countries = {"AF":"Afghanistan","AL":"Albania","DZ":"Algeria","AO":"Angola","AG":"Antigua and Barbuda","AR":"Argentina","AM":"Armenia","AU":"Australia","AT":"Austria","AZ":"Azerbaijan","BS":"Bahamas","BH":"Bahrain","BD":"Bangladesh","BB":"Barbados","BY":"Belarus","BE":"Belgium","BZ":"Belize","BJ":"Benin","BT":"Bhutan","BO":"Bolivia","BA":"Bosnia and Herzegovina","BW":"Botswana","BR":"Brazil","BN":"Brunei","BG":"Bulgaria","BF":"Burkina Faso","BI":"Burundi","KH":"Cambodia","CM":"Cameroon","CA":"Canada","CV":"Cape Verde","CF":"Central African Republic","TD":"Chad","CL":"Chile","CN":"China","CO":"Colombia","KM":"Comoros","CR":"Costa Rica","HR":"Croatia","CU":"Cuba","CY":"Cyprus","CZ":"Czech Republic","CD":"Democratic Republic of the Congo","DK":"Denmark","DJ":"Djibouti","DM":"Dominica","DO":"Dominican Republic","TL":"East Timor","EC":"Ecuador","EG":"Egypt","SV":"El Salvador","GQ":"Equatorial Guinea","ER":"Eritrea","EE":"Estonia","ET":"Ethiopia","FJ":"Fiji","FI":"Finland","FR":"France","GF":"French Guiana","GA":"Gabon","GM":"Gambia","GE":"Georgia","DE":"Germany","GH":"Ghana","GR":"Greece","GD":"Grenada","GP":"Guadeloupe","GT":"Guatemala","GN":"Guinea","GW":"Guinea-Bissau","GY":"Guyana","HT":"Haiti","HN":"Honduras","HU":"Hungary","IS":"Iceland","IN":"India","ID":"Indonesia","IR":"Iran","IQ":"Iraq","IE":"Ireland","IL":"Israel","IT":"Italy","CI":"Ivory Coast","JM":"Jamaica","JP":"Japan","JO":"Jordan","KZ":"Kazakhstan","KE":"Kenya","KI":"Kiribati","KW":"Kuwait","KG":"Kyrgyzstan","LA":"Laos","LV":"Latvia","LB":"Lebanon","LS":"Lesotho","LR":"Liberia","LY":"Libya","LT":"Lithuania","LU":"Luxembourg","MO":"Macao","MK":"Macedonia","MG":"Madagascar","MW":"Malawi","MY":"Malaysia","MV":"Maldives","ML":"Mali","MT":"Malta","MH":"Marshall Islands","MR":"Mauritania","MU":"Mauritius","MX":"Mexico","MD":"Moldova","MN":"Mongolia","ME":"Montenegro","MA":"Morocco","MZ":"Mozambique","MM":"Myanmar","NA":"Namibia","NR":"Nauru","NP":"Nepal","NL":"Netherlands","NZ":"New Zealand","NI":"Nicaragua","NE":"Niger","NG":"Nigeria","NO":"Norway","OM":"Oman","PK":"Pakistan","PW":"Palau","PA":"Panama","PG":"Papua New Guinea","PY":"Paraguay","PE":"Peru","PH":"Philippines","PL":"Poland","PT":"Portugal","PR":"Puerto Rico","QA":"Qatar","CG":"Republic of the Congo","RO":"Romania","RU":"Russia","RW":"Rwanda","KN":"Saint Kitts and Nevis","LC":"Saint Lucia","VC":"Saint Vincent and the Grenadines","WS":"Samoa","SM":"San Marino","ST":"Sao Tome and Principe","SA":"Saudi Arabia","SN":"Senegal","RS":"Serbia","SC":"Seychelles","SL":"Sierra Leone","SG":"Singapore","SK":"Slovakia","SI":"Slovenia","ZA":"South Africa","KR":"South Korea","ES":"Spain","LK":"Sri Lanka","SD":"Sudan","SR":"Suriname","SZ":"Swaziland","SE":"Sweden","CH":"Switzerland","TW":"Taiwan","TJ":"Tajikistan","TZ":"Tanzania","TH":"Thailand","TG":"Togo","TO":"Tonga","TT":"Trinidad and Tobago","TN":"Tunisia","TR":"Turkey","TM":"Turkmenistan","TV":"Tuvalu","UG":"Uganda","UA":"Ukraine","AE":"United Arab Emirates","GB":"United Kingdom","US":"United States","UY":"Uruguay","UZ":"Uzbekistan","VU":"Vanuatu","VE":"Venezuela","VN":"Vietnam","YE":"Yemen","ZM":"Zambia","ZW":"Zimbabwe"};
+var countries_curency = {
+    "AF": "AFN",
+    "AL": "ALL",
+    "DZ": "DZD",
+    "AO": "AOA",
+    "AG": "XCD",
+    "AR": "ARS",
+    "AM": "AMD",
+    "AU": "AUD",
+    "AT": "EUR",
+    "AZ": "AZN",
+    "BS": "BSD",
+    "BH": "BHD",
+    "BD": "BDT",
+    "BB": "BBD",
+    "BY": "BYN",
+    "BE": "EUR",
+    "BZ": "BZD",
+    "BJ": "XOF",
+    "BT": "BTN",
+    "BO": "BOB",
+    "BA": "BAM",
+    "BW": "BWP",
+    "BR": "BRL",
+    "BN": "BND",
+    "BG": "BGN",
+    "BF": "XOF",
+    "BI": "BIF",
+    "KH": "KHR",
+    "CM": "XAF",
+    "CA": "CAD",
+    "CV": "CVE",
+    "CF": "XAF",
+    "TD": "XAF",
+    "CL": "CLP",
+    "CN": "CNY",
+    "CO": "COP",
+    "KM": "KMF",
+    "CR": "CRC",
+    "HR": "HRK",
+    "CU": "CUC",
+    "CY": "EUR",
+    "CZ": "CZK",
+    "CD": "CDF",
+    "DK": "DKK",
+    "DJ": "DJF",
+    "DM": "XCD",
+    "DO": "DOP",
+    "TL": "USD",
+    "EC": "USD",
+    "EG": "EGP",
+    "SV": "USD",
+    "GQ": "XAF",
+    "ER": "ERN",
+    "EE": "EUR",
+    "ET": "ETB",
+    "FJ": "FJD",
+    "FI": "EUR",
+    "FR": "EUR",
+    "GF": "EUR",
+    "GA": "XAF",
+    "GM": "GMD",
+    "GE": "GEL",
+    "DE": "EUR",
+    "GH": "GHS",
+    "GR": "EUR",
+    "GD": "XCD",
+    "GP": "EUR",
+    "GT": "GTQ",
+    "GN": "GNF",
+    "GW": "XOF",
+    "GY": "GYD",
+    "HT": "HTG",
+    "HN": "HNL",
+    "HU": "HUF",
+    "IS": "ISK",
+    "IN": "INR",
+    "ID": "IDR",
+    "IR": "IRR",
+    "IQ": "IQD",
+    "IE": "EUR",
+    "IL": "ILS",
+    "IT": "EUR",
+    "CI": "XOF",
+    "JM": "JMD",
+    "JP": "JPY",
+    "JO": "JOD",
+    "KZ": "KZT",
+    "KE": "KES",
+    "KI": "AUD",
+    "KW": "KWD",
+    "KG": "KGS",
+    "LA": "LAK",
+    "LV": "EUR",
+    "LB": "LBP",
+    "LS": "LSL",
+    "LR": "LRD",
+    "LY": "LYD",
+    "LT": "EUR",
+    "LU": "EUR",
+    "MO": "MOP",
+    "MK": "MKD",
+    "MG": "MGA",
+    "MW": "MWK",
+    "MY": "MYR",
+    "MV": "MVR",
+    "ML": "XOF",
+    "MT": "EUR",
+    "MH": "USD",
+    "MR": "MRO",
+    "MU": "MUR",
+    "MX": "MXN",
+    "MD": "MDL",
+    "MN": "MNT",
+    "ME": "EUR",
+    "MA": "MAD",
+    "MZ": "MZN",
+    "MM": "MMK",
+    "NA": "NAD",
+    "NR": "AUD",
+    "NP": "NPR",
+    "NL": "EUR",
+    "NZ": "NZD",
+    "NI": "NIO",
+    "NE": "XOF",
+    "NG": "NGN",
+    "NO": "NOK",
+    "OM": "OMR",
+    "PK": "PKR",
+    "PW": "USD",
+    "PA": "PAB",
+    "PG": "PGK",
+    "PY": "PYG",
+    "PE": "PEN",
+    "PH": "PHP",
+    "PL": "PLN",
+    "PT": "EUR",
+    "PR": "USD",
+    "QA": "QAR",
+    "CG": "XAF",
+    "RO": "RON",
+    "RU": "RUB",
+    "RW": "RWF",
+    "KN": "XCD",
+    "LC": "XCD",
+    "VC": "XCD",
+    "WS": "WST",
+    "SM": "EUR",
+    "ST": "STD",
+    "SA": "SAR",
+    "SN": "XOF",
+    "RS": "RSD",
+    "SC": "SCR",
+    "SL": "SLL",
+    "SG": "BND",
+    "SK": "EUR",
+    "SI": "EUR",
+    "ZA": "ZAR",
+    "KR": "KRW",
+    "ES": "EUR",
+    "LK": "LKR",
+    "SD": "SDG",
+    "SR": "SRD",
+    "SZ": "SZL",
+    "SE": "SEK",
+    "CH": "CHF",
+    "TW": "TWD",
+    "TJ": "TJS",
+    "TZ": "TZS",
+    "TH": "THB",
+    "TG": "XOF",
+    "TO": "TOP",
+    "TT": "TTD",
+    "TN": "TND",
+    "TR": "TRY",
+    "TM": "TMT",
+    "TV": "AUD",
+    "UG": "UGX",
+    "UA": "UAH",
+    "AE": "AED",
+    "GB": "GBP",
+    "US": "USD",
+    "UY": "UYU",
+    "UZ": "UZS",
+    "VU": "VUV",
+    "VE": "VEF",
+    "VN": "VND",
+    "YE": "YER",
+    "ZM": "ZMW",
+    "ZW": "BWP"
+};
 
-var countries_curency = {"AF":"AFN","AL":"ALL","DZ":"DZD","AO":"AOA","AG":"XCD","AR":"ARS","AM":"AMD","AU":"AUD","AT":"EUR","AZ":"AZN","BS":"BSD","BH":"BHD","BD":"BDT","BB":"BBD","BY":"BYN","BE":"EUR","BZ":"BZD","BJ":"XOF","BT":"BTN","BO":"BOB","BA":"BAM","BW":"BWP","BR":"BRL","BN":"BND","BG":"BGN","BF":"XOF","BI":"BIF","KH":"KHR","CM":"XAF","CA":"CAD","CV":"CVE","CF":"XAF","TD":"XAF","CL":"CLP","CN":"CNY","CO":"COP","KM":"KMF","CR":"CRC","HR":"HRK","CU":"CUC","CY":"EUR","CZ":"CZK","CD":"CDF","DK":"DKK","DJ":"DJF","DM":"XCD","DO":"DOP","TL":"USD","EC":"USD","EG":"EGP","SV":"USD","GQ":"XAF","ER":"ERN","EE":"EUR","ET":"ETB","FJ":"FJD","FI":"EUR","FR":"EUR","GF":"EUR","GA":"XAF","GM":"GMD","GE":"GEL","DE":"EUR","GH":"GHS","GR":"EUR","GD":"XCD","GP":"EUR","GT":"GTQ","GN":"GNF","GW":"XOF","GY":"GYD","HT":"HTG","HN":"HNL","HU":"HUF","IS":"ISK","IN":"INR","ID":"IDR","IR":"IRR","IQ":"IQD","IE":"EUR","IL":"ILS","IT":"EUR","CI":"XOF","JM":"JMD","JP":"JPY","JO":"JOD","KZ":"KZT","KE":"KES","KI":"AUD","KW":"KWD","KG":"KGS","LA":"LAK","LV":"EUR","LB":"LBP","LS":"LSL","LR":"LRD","LY":"LYD","LT":"EUR","LU":"EUR","MO":"MOP","MK":"MKD","MG":"MGA","MW":"MWK","MY":"MYR","MV":"MVR","ML":"XOF","MT":"EUR","MH":"USD","MR":"MRO","MU":"MUR","MX":"MXN","MD":"MDL","MN":"MNT","ME":"EUR","MA":"MAD","MZ":"MZN","MM":"MMK","NA":"NAD","NR":"AUD","NP":"NPR","NL":"EUR","NZ":"NZD","NI":"NIO","NE":"XOF","NG":"NGN","NO":"NOK","OM":"OMR","PK":"PKR","PW":"USD","PA":"PAB","PG":"PGK","PY":"PYG","PE":"PEN","PH":"PHP","PL":"PLN","PT":"EUR","PR":"USD","QA":"QAR","CG":"XAF","RO":"RON","RU":"RUB","RW":"RWF","KN":"XCD","LC":"XCD","VC":"XCD","WS":"WST","SM":"EUR","ST":"STD","SA":"SAR","SN":"XOF","RS":"RSD","SC":"SCR","SL":"SLL","SG":"BND","SK":"EUR","SI":"EUR","ZA":"ZAR","KR":"KRW","ES":"EUR","LK":"LKR","SD":"SDG","SR":"SRD","SZ":"SZL","SE":"SEK","CH":"CHF","TW":"TWD","TJ":"TJS","TZ":"TZS","TH":"THB","TG":"XOF","TO":"TOP","TT":"TTD","TN":"TND","TR":"TRY","TM":"TMT","TV":"AUD","UG":"UGX","UA":"UAH","AE":"AED","GB":"GBP","US":"USD","UY":"UYU","UZ":"UZS","VU":"VUV","VE":"VEF","VN":"VND","YE":"YER","ZM":"ZMW","ZW":"BWP"};
-  
- $.ajaxSetup({
+$.ajaxSetup({
     async: false
 });
 
-var country,currencies,city;
+var country, currencies, city;
 
-$.getJSON("Contry.json", function( data ) {
-    country= data;    
+$.getJSON("Contry.json", function (data) {
+    country = data;
 });
 
-$.getJSON("currencies.json", function( data ) {
-    currencies= data;  
+$.getJSON("currencies.json", function (data) {
+    currencies = data;
 });
 
-$.getJSON("City.json", function( data ) {
-    city = data;    
+$.getJSON("City.json", function (data) {
+    city = data;
 });
 
- $.ajaxSetup({
+$.ajaxSetup({
     async: true
 });
 //console.log(city);
 
-$(document).ready(function() {
-   // console.log(" Germany"==="Germany")
+$(document).ready(function () {
 
     var user_coutries_data = {};
 
     var items = {};
     var data = countries_curency;
-    $.each(countries_curency, function(key, val) {
+    $.each(countries_curency, function (key, val) {
 
         if (val in currencies) {
             var s = countries[key] + ' - ' + currencies[val]["name"];
@@ -46,7 +424,8 @@ $(document).ready(function() {
     });
     user_coutries_data = items;
 
-    var country1;
+    var country1, contry_city1;
+
     $('#user_countries_option').flagStrap({
         countries: user_coutries_data,
         buttonSize: "btn-sm",
@@ -58,19 +437,32 @@ $(document).ready(function() {
             value: "",
             text: "Please select a country"
         },
-         onSelect: function (value, element) {
-             country1 = $(element).children("option[selected=selected]").val(); //This is the correct value
+        onSelect: function (value, element) {
+            country1 = $(element).children("option[selected=selected]").val(); //This is the correct value
 
-     console.log("This is the selected country code: " + country1);
-             // console.log(country1);
+            console.log("This is the selected country code: " + country1);
+
+            contry_city1 = countries[country1];
+
+            $("#select1").html(" <option>Peas select the city</option>");
+            $.each(city, function (key, val) {
+                if (val['Country'] == contry_city1){
+                    $("#select1").append("<option>"+key+"</option>");
+                }});
         }
     });
-     
+
     country1 = $("#user_countries_option > select").children("option[selected=selected]").val();
- //console.log(country1);
+
+    contry_city1 = countries[country1];
+
+    $.each(city, function (key, val) {
+        if (val['Country'] == contry_city1){
+            $("#select1").append("<option>"+key+"</option>");
+        }});
 
 
-    var country2;
+    var country2,contry_city2;
     $('#interest_countries_option').flagStrap({
         countries: countries,
         buttonSize: "btn-sm",
@@ -85,14 +477,27 @@ $(document).ready(function() {
         onSelect: function (value, element) {
             country2 = $(element).children("option[selected=selected]").val();
             console.log("This is the selected country code: " + country2);
-           
+            contry_city2 = countries[country2];
+
+            $("#select2").html(" <option>Peas select the city</option>");
+            $.each(city, function (key, val) {
+                if (val['Country'] == contry_city2){
+                    $("#select2").append("<option>"+key+"</option>");
+                }});
+            $('#contry_1').html(contry_city2);
         }
     });
-    
-    country2 = $("#interest_countries_option > select").children("option[selected=selected]").val();
- 
 
-    $('form').submit(function(e){
+    country2 = $("#interest_countries_option > select").children("option[selected=selected]").val();
+    contry_city2 = countries[country2];
+
+    $.each(city, function (key, val) {
+        if (val['Country'] == contry_city1){
+            $("#select2").append("<option>"+key+"</option>");
+        }});
+    $('#contry_1').html(contry_city2);
+
+    $('form').submit(function (e) {
         e.preventDefault();
 
         var cur1 = countries_curency[country1];
@@ -109,22 +514,22 @@ $(document).ready(function() {
         $.ajax({
             url: 'calc.php',
             data: {
-                currency1 : cur1,
-                user_country_ppp : country1,
-                currency2 : cur2,
-                interest_country_ppp : country2,
-                amount : amount
+                currency1: cur1,
+                user_country_ppp: country1,
+                currency2: cur2,
+                interest_country_ppp: country2,
+                amount: amount
             },
             type: 'POST',
-            success: function(json){
-                
+            success: function (json) {
+
                 var response = jQuery.parseJSON(json);
                 if (response.status == '1') {
 
                     var currency_symbol = '',
                         currency1_name = cur1,
                         currency2_name = cur2;
-                   
+
                     if (currencies[cur1]) {
                         currency_symbol = currencies[cur1]["symbol"];
                         currency1_name = currencies[cur1]["name"];
@@ -139,9 +544,9 @@ $(document).ready(function() {
                     }
                     var message = "At this moment you can buy with <b>" + amount + " " + currency1_name + "</b> " + "about as much in " + countries[country2] + " as you could buy with about <div class='text-center'> <button id=\"reset\" class=\"btn btn-primary\"disabled=\"disabled\">Reset to avetage <br>value</button><span class='koef-ppp'>" + response.result + "  " + currency_symbol + '<a href="#" data-toggle="tooltip"data-html="true" data-placement="right" title="Explained: We convert the amount to local currency and divide by the so-called „PPP-Factor“ (Purchasing Power Parity), issued by the International Monetary Fund. This returns an amount in Dollars that has the same purchasing power in the USA as the originally entered amount would have in the foreign country’s currency; we then finally multiply this amount with the PPP of your country, which returns the amount in your currency that equals the purchasing power of the calculatory Dollar amount"><b>?</b></a></span><button id=\"result\" class=\"btn btn-primary\">Specify Result</button></div><br> in ' + the + countries[country1] + ".";
                     $('#resultMessage').html(message);
-                    
+
                 }
-                 else {
+                else {
                     $('#resultMessage').html(response.result);
                     console.log(response.result);
                 }
@@ -150,9 +555,5 @@ $(document).ready(function() {
             }
         });
     });
- 
-});
 
-$("#result").click(function(){
-    $(this).console.log("uhgjhfghj");
 });
